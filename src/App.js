8a330a98aab5f181b4f7029e045ae33d826c823f
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-10-15 17:53:46 -0700

    completed lab 28, need UML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Form from './components/form/form';
 import Results from './components/results/results';
+import axios from 'axios';
+
 
 function App() {
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
-  const callApi = (requestParams) => {
-    // mock output
-    const payload = {
-      count: 2,
-      results: [
-        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-      ],
-    };
-    setData(payload);
-    setRequestParams(requestParams);
+
+
+  const callApi = (receivingObjOnForm) => {
+    setRequestParams(receivingObjOnForm);//updating/setting the state
   }
+  useEffect(() => {
+    async function gatherData() {
+      if (requestParams.method === 'GET') {
+        await axios.get(requestParams.url).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+      if (requestParams.method === 'POST') {
+        await axios.post(requestParams.url, requestParams.body).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+      if (requestParams.method === 'PUT') {
+        await axios.put(requestParams.url).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+      if (requestParams.method === 'DELETE') {
+        await axios.delete(requestParams.url).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+
+    }
+    gatherData();
+
+
+  }, [requestParams]);
+
+
+
   return (
     <React.Fragment>
       <Header />
